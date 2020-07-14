c1d7b097d8a1b139714e978534610d5869cc2c9f
DanielHa <54295381+DonHa1311@users.noreply.github.com>
2020-07-14 14:22:45 -0500

    Update to new version
    Add: Function to print the available passwords to a file

--- checkPass.py
+++ checkPass.py
@@ -1,3 +1,5 @@
+# Author: Daniel Ha
+# This programm check how many times does your password got hacked by using pwedpasswords.com
 import requests
 import hashlib
 import sys
@@ -25,19 +27,35 @@ def pwned_api_check(password):
     response = request_api_data(first5_char)
     return get_password_leaks_count(response, tail)
 
-
-def main(args):
-    for password in args:
-        count = pwned_api_check(password)
-        if len(password) < 8:
-            print(f'{password} is too short. Please try again!!!')
-        elif count:
-            print(f'{password} was found {count} times ... you should change your password')
-        else:
-            print(f'{password} was not found. You can use this password')
+def print_available_password(pass_list):
+    f = open('available_password.txt', 'a')
+    for password in pass_list:
+        f.write(password)
+    f.close()
     return 0
 
+def main(args = 'passwords.txt'):
+    f = open(args, 'r')
+    available_pass = []
+    for password in f:
+        new_pass = password.replace('\n', '')
+        count = pwned_api_check(new_pass)
+        if len(new_pass) < 8:
+            print(f'{new_pass} is too short. Password need to be at least 8 characters!!!')
+        elif count and int(count) < 10:
+            if int(count) == 1:
+                print(f'{new_pass} was found {count} time ... you might want to change your password')
+            else:
+                print(f'{new_pass} was found {count} times ... you might want to change your password')
+        elif int(count) >= 10:
+            print(f'{new_pass} was found {count} times ... you should change your password')
+        else:
+            print(f'{new_pass} was not found. You can use this password')
+            available_pass.append(password)
+    f.close()
+    return print_available_password(available_pass)
+
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main(sys.argv[1])
 
 
